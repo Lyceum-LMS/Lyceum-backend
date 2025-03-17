58f14d49d6c1b6c94885881949256485c788d2c4
LeSci-0x1 <sscire@csumb.edu>
2025-03-17 14:14:03 -0700

    This commit is made up of two parts.
    Part 1
    "Complete entity classes for enrollment, assignment, and grade"
    Part 1 of this commit implements the Assignment, Enrollment and Grade entities and their corresponding Repositories.
    
    Assignment (entity)
    Basic Requirements Implemented:
    1. an int valued primary key generated by the database
    2. a String valued attribute of 'title'
    3. a Data (java.sql.Date) valued attribute for 'dueDate'
    4. ManyToOne relationship to Section entity
    5. add getters and setter methods
    
    AssignmentRepository
    Basic Requirements Implemented:
    1. uncomment all the lines for each Query
    
    Enrollment (entity)
    Basic Requirements Implemented:
    1. an int valued primary key generated by the database.
    2. a String attribute for the final grade that student receives for a course.
       The value will be null until instructor enters the final grades.
    3. ManyToOne relationship to Section entity
    4. Many to One relationship to User entity
    5. add getters and setter methods
    
    EnrollmentRepository
    Basic Requirements Implemented:
    1. uncomment all the lines for each Query
    
    Grade (entity)
    (this is for an assignment grade, not the final course grade ).
    Every enrolled student can have a Grade for every assignment.
    Basic Requirements Implemented:
    1. an int valued primary key generated by database.
    2. an Integer (use the Integer class, not “int”) attribute for the student’s assignment score.
       This may be a null value if instructor has not entered scores.
    3. ManyToOne relationship to Assignment
    4. ManyToOne relationship to Enrollment
    5. add getters and setter methods
    
    GradeRepository
    Basic Requirements Implemented:
    1. uncomment all the lines for each Query
    
     Additional implementations:
    	- none required outside of Basic Requirements
     data validation:
    	- none required outside of Basic Requirements
    
    Part 2
    StudentController
    Part 2 of this commit implements the functionality for the StudentController based on the starter code.
    Basic Requirements Implemented:
    1. GET /enrollments: Students lists there enrollments given year and semester value; returns list of enrollments, may be empty
    2. GET /assignments: Students lists there assignments given year and semester value; returns list of assignments may be empty
    
    Additional implementations:
     Added student access validation
    	- logged in user must be the student (assignment 7)
    
     data validation:
       - studentId existence validation to prevent extraneous requests (#1, #2)
       - year/semester existence validation (validated as a group) to prevent extraneous requests (#1, #2)
       - Grade existence check before assigning grade.score to prevent NullPointerException error (#2)

--- src/main/java/com/cst438/domain/Enrollment.java
+++ src/main/java/com/cst438/domain/Enrollment.java
@@ -20,7 +20,7 @@ public class Enrollment {
 
     // create relationship between enrollment and user entities
     @ManyToOne
-    @JoinColumn(name="id", nullable=false)
+    @JoinColumn(name="user_id", nullable=false)
     private User student;
     // create relationship between enrollment and section entities
     @ManyToOne
