1066673778e24fb108ae443955cc7266c5fc4b7a
LeSci-0x1 <sscire@csumb.edu>
2025-04-01 19:42:08 -0700

    -add records to data.sql
    -update thrown exceptions in StudentScheduleController
    
    Assignment 4
    
    This commit is made up of several parts.
    
    InstructorControllerClassGradesSystemTest
    Part 1
    "System test to enter enrollment grades for enrolled students"
    Part 1 of this commit implements the Selenium system test for the Instructor components - InstructorHome and InstructorSectionsView
    
    Rubric Requirements Implemented:
    1. The test uses Selenium to navigate from the home page for an instructor, to view a list of Sections,
    2. enter year and semester and view the list of sections,
    3. then select the link to view enrollments,
    4. and the grade field for each enrolled student is updated with a final letter grade value.
    5. Then the grades are saved.
    6. There are assert statements to verify that the grades were saved.
    
    EnrollmentControllerStudentEnrollSectionSystemTest
    Part 2
    "System test to enroll into a section"
    Part 2 of this commit implements the Selenium system test for the Student components - CourseEnroll and ScheduleView
    
    Rubric Requirements Implemented:
    1. The test uses Selenium to navigate from the home page for an student to the page to enroll into a section.
    2. A section is selected from the list of open sections.
    3. The student view schedule page is selected, and the year and semester are entered.
    4. There are assert statements that verify the new section was successfully added to the student's schedule.
    
    EnrollmentControllerEnrollSectionUnitTest
    Part 3
    "Unit test to enroll into a section"
    Part 3 of this commit implements a Unit Test for Student components - StudentScheduleController - addCourse()
    
    Rubric Requirements Implemented:
    1. Unit test invokes REST api POST /enrollments/sections/{sectionNo}?studentId={id}.
    2. The request is successful and the test asserts that the returned status code is 200 (ok) and that the returned EnrollmentDTO data has expected data.
    
    Part 4
    "Unit test to enroll that fails due to duplicate course"
    Part 4 of this commit implements a Unit Test for Student components - StudentScheduleController - addCourse()
    
    Rubric Requirements Implemented:
    1. Unit test invokes REST api POST /enrollments/sections/{sectionNo}?studentId={id}.
    2. The request is unsuccessful because the student is already enrolled.
    3. There are assert statements on the returned status code and error message.
    
    Part 5
    "Unit test to enroll that has bad section number"
    Part 5 of this commit implements a Unit Test for Student components - StudentScheduleController - addCourse()
    
    Rubric Requirements Implemented:
    1. Unit test invokes REST api POST /enrollments/sections/{sectionNo}?studentId={id}.
    2. The request is unsuccessful due to an invalid section number.
    3. The tests contain assert statements for a bad status code and error message.
    
    Part 6
    "Unit test to enroll into a course that is past add deadline"
    Part 6 of this commit implements a Unit Test for Student components - StudentScheduleController - addCourse()
    
    Rubric Requirements Implemented:
    1. Unit test invokes REST api POST /enrollments/sections/{sectionNo}?studentId={id}.
    2. The request is unsuccessful because the date is past the add deadline for the section.
    3. The test has assert statements that check for bad status code and error message.
    
    Part 7
    "Unit test to update enrollment grade"
    Part 7 of this commit implements a Unit Test for Instructor components - EnrollmentController - getEnrollments, updateEnrollmentGrade
    
    Rubric Requirements Implemented:
    1. Unit test invokes REST api GET for the url /sections/{sectionNo}/enrollments.
    2. Update the returned list of EnrollmentDTO objects with grades and the invokes PUT /enrollments with a body containing the updates EnrollmentDTO objects.
    3. The request is successful, and the test contains asserts for the status code.

--- src/test/java/com/cst438/controller/EnrollmentControllerSystemTest.java
+++ src/test/java/com/cst438/controller/EnrollmentControllerSystemTest.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class InstructorControllerSystemTestSLS {
+public class EnrollmentControllerSystemTest {
 
     // TODO edit the following to give the location and file name
     // of the Chrome driver.
